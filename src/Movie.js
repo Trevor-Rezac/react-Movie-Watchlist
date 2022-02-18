f8f70e64c4735f0a18ec4077dd28243758534c3d
TrevorRezac <trev.rezac@gmail.com>
2022-02-18 14:04:27 -0800

    minor styling for visibility

--- src/Movie.js
+++ src/Movie.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Movie({ movie }) {
   return (
-    <div>
+    <div className='movie-poster'>
       <h3>{movie.title}</h3>
       <p>{movie.overview}</p>
       <img src={movie.poster_path ? `https://image.tmdb.org/t/p/original/${movie.poster_path}` : 'https://www.placecage.com/gif/200/300'}/>
--- src/MovieList.js
+++ src/MovieList.js
@@ -4,7 +4,7 @@ import Movie from './Movie';
 export default function MovieList({ movies }) {
   
   return (
-    <div>
+    <div className='movie-list'>
       {movies.map((movie, i) => <Movie key={`${movie}-${i}`} movie={movie}/>)}
     </div>
   
