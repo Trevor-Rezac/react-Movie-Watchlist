65290ee97085a91e7f383e44ed7279c866c652d8
TrevorRezac <trev.rezac@gmail.com>
2022-02-18 10:50:46 -0800

    built authpage form, signUp works correctly

--- src/AuthPage.js
+++ src/AuthPage.js
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { signUp } from './services/fetch-utils';
 
-export default function AuthPage() {
+export default function AuthPage({ setCurrentUser }) {
+  const [emailForm, setEmailForm] = useState('');
+  const [passwordForm, setPasswordForm] = useState('');
+
+  async function handleSignUp(e) {
+    e.preventDefault();
+
+    const user = await signUp(emailForm, passwordForm);
+    setCurrentUser(user);
+    setEmailForm('');
+    setPasswordForm('');
+  }
+
   return (
-    <h3>AuthPage</h3>
+    <>
+      <h3>AuthPage</h3>
+      <div className='form-container'>
+        <form>
+          <input placeholder='Email'
+            type='email'
+            value={emailForm}
+            onChange={(e) => setEmailForm(e.target.value)}
+          />
+          <input placeholder='Password'
+            type='password'
+            value={passwordForm}
+            onChange={(e) => setPasswordForm(e.target.value)}
+          />
+          <button>Sign In</button>
+          <button onClick={handleSignUp}>Sign Up</button>
+        </form>
+      </div>
+    </>
   );
 }
