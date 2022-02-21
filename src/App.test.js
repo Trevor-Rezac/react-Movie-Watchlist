fc2aee9e0bb96af991797dfb7adf33aa7863d577
TrevorRezac <trev.rezac@gmail.com>
2022-02-21 10:09:06 -0800

    one passing test

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,13 @@
-// import { render, screen } from '@testing-library/react';
-// import SearchPage from './SearchPage';
-// import { BrowserRouter as Router } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import SearchPage from './SearchPage';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 
-// test('renders a header on searchPage', async () => {
-//   const location = { pathname: '/search-page' };
-//   render(<Router history={location}>
-//     <SearchPage /> 
-//   </Router>);
-//   const linkElement = await screen.findByText(/Add movies to your Watchlist!/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+test('renders a header on searchPage', async () => {
+  const location = { pathname: '/search-page' };
+  render(<Router history={location}>
+    <SearchPage /> 
+  </Router>);
+  const linkElement = await screen.findByText(/Add movies to your Watchlist!/i);
+  expect(linkElement).toBeInTheDocument();
+});
